a90044b5561422f878f88d13b090a5add2ed38e0
kishor-kalane1961 <kishorkalane2005@gmail.com>
2026-08-08 01:35:02 +0530

    Added HomePage and updated Main class

--- ecoload/src/main/java/com/super_x/Main.java
+++ ecoload/src/main/java/com/super_x/Main.java
@@ -1,7 +1,11 @@
 package com.super_x;
+import com.super_x.view.HomePage;
+
+import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Application.launch(HomePage.class, args);
     }
 }
